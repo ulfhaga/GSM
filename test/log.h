320ba0d0ab239b39c51aff0a6f9e5d6dbbd49240
ulfhaga <ulf.haga@gmail.com>
2019-01-20 16:27:15 +0100

    Save

--- test/log.h
+++ test/log.h
@@ -42,23 +42,23 @@ extern const char * log_level_strings [];
 
 #define LOG_DEBUG(format, ...) if ( IS_LOGGING(LOG_LVL_DEBUG) ) {      \
   fprintf(LOG_FP,"[%s] %s:%d ", log_level_strings[LOG_LVL_DEBUG],  __FUNCTION__, __LINE__) ;\
-  fprintf (LOG_FP,format "\n", __VA_ARGS__ ) ;};
+  fprintf (LOG_FP,format "\n",## __VA_ARGS__ ) ;};
 
 #define LOG_LOG(format, ...) if ( IS_LOGGING(LOG_LVL_DEBUG) ) {        \
   fprintf(LOG_FP,"[%s] %s:%d ", log_level_strings[LOG_LVL_DEBUG],  __FUNCTION__, __LINE__) ;\
-  fprintf (LOG_FP,format "\n", __VA_ARGS__ ) ;};
+  fprintf (LOG_FP,format "\n",## __VA_ARGS__ ) ;};
 
 #define LOG_NOTICE(format, ...) if ( IS_LOGGING(LOG_LVL_NOTICE) ) {    \
   fprintf(LOG_FP,"[%s] %s:%d ", log_level_strings[LOG_LVL_NOTICE],  __FUNCTION__, __LINE__) ;\
-  fprintf (LOG_FP,format "\n", __VA_ARGS__ ) ;};
+  fprintf (LOG_FP,format "\n",## __VA_ARGS__ ) ;};
 
 #define LOG_WARNING(format, ...) if ( IS_LOGGING(LOG_LVL_WARNING) ) {  \
   fprintf(LOG_FP,"[%s] %s:%d ", log_level_strings[LOG_LVL_WARNING],  __FUNCTION__, __LINE__) ;\
-  fprintf (LOG_FP,format "\n", __VA_ARGS__ ) ;};
+  fprintf (LOG_FP,format "\n",## __VA_ARGS__ ) ;};
 
 #define LOG_CRITICAL(format, ...) if ( IS_LOGGING(LOG_LVL_CRITICAL) ) {\
   fprintf(LOG_FP,"[%s] %s:%d ", log_level_strings[LOG_LVL_CRITICAL],  __FUNCTION__, __LINE__) ;\
-  fprintf (LOG_FP,format "\n", __VA_ARGS__ ) ;};
+  fprintf (LOG_FP,format "\n",## __VA_ARGS__ ) ;};
 
 #endif // __log_h__
 
